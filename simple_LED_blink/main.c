1c5fb0d0afdc9b7e68d547f7c76c3039c675a07d
Srećko Lazić <sreckolazic94@gmail.com>
2024-09-09 19:26:16 +0200

    New file implement digitalWrite and pinMode similar to Arduino IDE

--- simple_LED_blink/main.c
+++ simple_LED_blink/main.c
@@ -1,17 +1,29 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define SUCCESS 0
+#include "../common_types.h"
+
+int OUTPUT = 0x1;
+int HIGH = 0x1;
+int LOW = 0x0;
 
 int main(void) {
 	int loop_flag = 1;
+
 	// Sets pin 13 (PB5) as output
-	DDRB |= (1 << DDB5);
+	//DDRB |= (1 << DDB5);
+	setPinMode(12, OUTPUT);
 
 	while(loop_flag) {
 		// Toggle LED
-		PORTB ^= (1 << PORTB5);
-		_delay_ms(1000);
+		//PORTB ^= (1 << PORTB5);
+		
+		//PORTB ^= (1 << PORTB4);
+		digitalPinWrite(12, HIGH);
+		_delay_ms(2500);
+
+		digitalPinWrite(12, LOW);
+		_delay_ms(500);
 	}
 	
 	return SUCCESS;
